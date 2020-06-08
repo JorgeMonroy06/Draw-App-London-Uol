76154d3672803947fc62786b4258889a20ca9fb3
osmancalisir <28446823+osmancalisir@users.noreply.github.com>
2020-06-08 18:07:22 +0300

    University of London CM1010 - Draw Application

---
+++ Solved/sprayCanTool.js
@@ -0,0 +1,21 @@
+function sprayCanTool(){
+	this.icon = "assets/sprayCan.jpg";
+	this.name = "sprayCan";
+    this.points = 13;
+    this.spread = 10;
+    
+    this.draw = function(){
+                //if the mouse is pressed paint on the canvas
+                //spread describes how far to spread the paint from the mouse pointer
+                //points holds how many pixels of paint for each mouse press.
+                if(mouseIsPressed){
+                    for(var i = 0; i < this.points; i++){
+                        point(random(mouseX-this.spread, mouseX + this.spread), 
+                            random(mouseY-this.spread, mouseY+this.spread));
+                        
+                    }
+                }
+            }
+        
+    }
+
